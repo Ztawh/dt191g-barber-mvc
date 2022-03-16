e6eea8c798ea937d2ef4bca38f1d60ac57fc6d10
Amanda Hwatz Björkholm <amandahwatz@gmail.com>
2022-03-16 19:57:51 +0100

    Add styling

--- Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -115,7 +115,8 @@ namespace barber_mvc.Areas.Identity.Pages.Account
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
-                    return LocalRedirect(returnUrl);
+                    // return LocalRedirect(returnUrl);
+                    return LocalRedirect("~/Appointment");
                 }
                 if (result.RequiresTwoFactor)
                 {
